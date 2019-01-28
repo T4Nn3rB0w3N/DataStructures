6f7636cbc07c0d1135025dc0aecfafac3cd7fb4b
Bowen <tbow1382@740s-pl-120.local>
2019-01-28 09:00:38 -0700

    changes
    - deleted main.cpp
    - created Controller.cpp and Controller.hpp
    - created "Diagrams", "Controller", "Model", and "Resources" groups

---
+++ DataStructures/Controller/Controller.cpp
@@ -0,0 +1,14 @@
+//
+//  Controller.cpp
+//  DataStructures
+//
+//  Created by Bowen, Tanner on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+void Controller :: start()
+{
+    
+}
