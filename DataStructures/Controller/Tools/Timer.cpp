8925e0104d64d9289360de59336be07ab4c2fa1b
Bowen <tbow1382@740s-pl-120.local>
2019-02-05 08:57:15 -0700

    Created Timer.cpp and Timer.hpp, and added them to a new folder, "Tools", in the Controller group.

---
+++ DataStructures/Controller/Tools/Timer.cpp
@@ -0,0 +1,9 @@
+//
+//  Timer.cpp
+//  DataStructures
+//
+//  Created by Bowen, Tanner on 2/5/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Timer.hpp"
---
+++ DataStructures/Controller/Tools/Timer.hpp
@@ -0,0 +1,14 @@
+//
+//  Timer.hpp
+//  DataStructures
+//
+//  Created by Bowen, Tanner on 2/5/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Timer_hpp
+#define Timer_hpp
+
+#include <stdio.h>
+
+#endif /* Timer_hpp */
