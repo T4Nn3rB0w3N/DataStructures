ff18a0c7115560d1734c594f7268d9153ff739e5
Bowen <tbow1382@740s-pl-120.local>
2019-02-05 09:27:40 -0700

    Finished with FileController.hpp
    The code for FileController.hpp is completely written.

--- DataStructures/Controller/FileController.hpp
+++ DataStructures/Controller/FileController.hpp
@@ -9,6 +9,20 @@
 #ifndef FileController_hpp
 #define FileController_hpp
 
-#include <stdio.h>
+#include "../Resources/CrimeData.hpp"
+#include "../Resources/Music.hpp"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
 
+using namespace std;
+
+class FileController
+{
+public:
+    static vector<CrimeData> readCrimeDataToVector(string filename);
+    static vector<Music> musicDataToVector(string filename);
+};
 #endif /* FileController_hpp */
