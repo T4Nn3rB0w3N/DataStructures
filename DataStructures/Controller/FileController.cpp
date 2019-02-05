3e5e58a51806d845819ec20abf4d1db317026a43
Bowen <tbow1382@740s-pl-120.local>
2019-02-05 09:21:37 -0700

    created FileController.cpp and FileController.hpp.

---
+++ DataStructures/Controller/FileController.cpp
@@ -0,0 +1,9 @@
+//
+//  FileController.cpp
+//  DataStructures
+//
+//  Created by Bowen, Tanner on 2/5/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "FileController.hpp"
---
+++ DataStructures/Controller/FileController.hpp
@@ -0,0 +1,14 @@
+//
+//  FileController.hpp
+//  DataStructures
+//
+//  Created by Bowen, Tanner on 2/5/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef FileController_hpp
+#define FileController_hpp
+
+#include <stdio.h>
+
+#endif /* FileController_hpp */
