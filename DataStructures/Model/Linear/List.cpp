a71753080dc4caffec4484e15996fe317eaaec0a
Bowen <tbow1382@740s-pl-120.local>
2019-02-07 09:28:55 -0700

    Created List.cpp and wrote its header

---
+++ DataStructures/Model/Linear/List.cpp
@@ -0,0 +1,16 @@
+//
+//  List.cpp
+//  DataStructures
+//
+//  Created by Bowen, Tanner on 2/7/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "List.hpp"
+
+using namespace std; //Used for keyword access. Use this to make sure your pointers are pointing to the right things.
+
+#ifndef LinkedList_hpp
+#define LinkedList_hpp
+
+
