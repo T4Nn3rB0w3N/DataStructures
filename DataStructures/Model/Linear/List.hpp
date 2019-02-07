0b990e1cf817240243a335e0669a9c3eabe65935
Bowen <tbow1382@740s-pl-120.local>
2019-02-07 08:34:41 -0700

    The class for List.hpp is now completed.

--- DataStructures/Model/Linear/List.hpp
+++ DataStructures/Model/Linear/List.hpp
@@ -16,6 +16,16 @@ class List
 {
 protected:
     int size;
+public:
+    //Structure
+    virtual void add(Type item) = 0;
+    virtual void addAtIndex(int index, Type item) = 0;
+    virtual Type remove(int index) = 0;
+    virtual Type getFromIndex(int index) = 0;
+    //Helper / Getters
+    virtual int getSize() const = 0;
+    virtual LinearNode<Type> * getFront() = 0;
+    virtual LinearNode<Type> * getEnd() = 0;
 };
 
 #endif /* List_hpp */
