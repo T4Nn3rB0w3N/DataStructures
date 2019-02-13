4f8466dff24721884fdfa232a1dffe7a05d0a13a
Bowen <tbow1382@740s-pl-120.local>
2019-02-13 09:43:23 -0700

    Created LinearTester (.cpp & .hpp), Changes in LinkedList.hpp
    LinkedList.hpp:
    Added more templates.

---
+++ DataStructures/Testers/LinearTester.cpp
@@ -0,0 +1,14 @@
+//
+//  LinearTester.cpp
+//  DataStructures
+//
+//  Created by Bowen, Tanner on 2/13/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "LinearTester.hpp"
+
+void LinearTester :: testVsSTL()
+{
+    
+}
