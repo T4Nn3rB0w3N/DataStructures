ce1ff5e84c7113280a0e293dc699a52bfdcff08f
Bowen <tbow1382@740s-pl-120.local>
2019-03-06 09:49:48 -0700

    Created Graph.hpp and wrote its header section.
    Graph.hpp:
    header section is entirely written.

---
+++ DataStructures/Model/NonLinear/Graph.hpp
@@ -0,0 +1,58 @@
+//
+//  Graph.hpp
+//  DataStructures
+//
+//  Created by Bowen, Tanner on 3/6/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Graph_hpp
+#define Graph_hpp
+
+#include <set>
+#include <queue>
+#include <assert.h>
+
+using namespace std;
+template <class Type>
+Class Graph
+{
+private:
+    static const int MAXIMUM = 50;
+    bool adfacencyMatrix [MAXIMUM][MAXIMUM];
+    int weightCostMatrix [MAXIMUM][MAXIMUM];
+    Type graphData[MAXIMUM];
+    int vertexCount;
+    void depthFirstTraversal(Graph<Type> & graph, int vertex, bool markedVertices[]);
+public:
+    Graph();
+    //Graph operations
+    
+    void addVertex(const Type& value);
+    
+    //Connect vertices
+    void addEdge(int source, int target);
+    void addEdgeUndirected(int source, int target);
+    void addEdgeCost(int source, int target, int cost);
+    //Disconnect vertices
+    void removeEdge(int source, int target);
+    void removeEdgeUndirected(int source, int target);
+    void removeEdgeCost(int source, int target);
+    
+    //Accessors
+    Type& operator [] (int vertex);
+    Type operator [] (int vertex) const;
+    int size() const;
+    
+    //Check connections
+    bool hasUndirectedConnection(int source, int target) const;
+    bool areConnected(int source, int target) const;
+    std::set<int> neighbors(int vertex) const;
+    
+    //Traversals
+    void depthFirstTraversal(Graph<Type> & graph, int vertex);
+    void breadthFirstTraversal(Graph<Type> & graph, int vertex);
+    int costTraversal(Graph<Type> & graph, int vertex);
+};
+
+#endif /* Graph_hpp */
