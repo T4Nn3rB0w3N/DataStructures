7b5a49bd5b283fc0eb73309509197a5865c61886
Bowen <tbow1382@740s-pl-120.local>
2019-04-12 09:13:09 -0600

    Created BinaryTreeTester (.cpp & .hpp).

---
+++ DataStructures/Testers/BinaryTreeTester.cpp
@@ -0,0 +1,9 @@
+//
+//  BinaryTreeTester.cpp
+//  DataStructures
+//
+//  Created by Bowen, Tanner on 4/12/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "BinaryTreeTester.hpp"
---
+++ DataStructures/Testers/BinaryTreeTester.hpp
@@ -0,0 +1,14 @@
+//
+//  BinaryTreeTester.hpp
+//  DataStructures
+//
+//  Created by Bowen, Tanner on 4/12/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef BinaryTreeTester_hpp
+#define BinaryTreeTester_hpp
+
+#include <stdio.h>
+
+#endif /* BinaryTreeTester_hpp */
