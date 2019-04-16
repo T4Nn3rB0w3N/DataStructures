bbf03461496d00c7fd4c4047f39545b51478b728
Bowen <tbow1382@740s-pl-120.local>
2019-04-16 08:42:22 -0600

    Created BinarySearchTree.hpp and wrote the entire protected section for it.

---
+++ DataStructures/Model/NonLinear/BinarySearchTree.hpp
@@ -0,0 +1,37 @@
+//
+//  BinarySearchTree.hpp
+//  DataStructures
+//
+//  Created by Bowen, Tanner on 4/16/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef BinarySearchTree_hpp
+#define BinarySearchTree_hpp
+
+#include "Tree.hpp"
+#include <iostream>
+
+using namespace std;
+
+template <class Type>
+class BinarySearchTree : public Tree<Type>
+{
+protected:
+    //MARK: Protected Methods
+    int calculateSize(BinaryTreeNode<Type> * startNode);
+    int calculateHeight(BinaryTreeNode<Type> * startNode);
+    bool isBalanced(BinaryTreeNode<Type> * startNode);
+    bool isComplete(BinaryTreeNode<Type> * startNode);
+    
+    void inOrderTraversal(BinaryTreeNode<Type> * inStart);
+    void preOrderTraversal(BinaryTreeNode<Type> * preStart);
+    void postOrderTraversal(BinaryTreeNode<Type> * postStart);
+    
+    void destroyTree(BinaryTreeNode<Type> * node);
+    
+    BinaryTreeNode<Type> * getRightMostChild(BinaryTreeNode<Type> * current);
+    BinaryTreeNode<Type> * getLeftMostChild(BinaryTreeNode<Type> * current);
+}
+
+#endif /* BinarySearchTree_hpp */
