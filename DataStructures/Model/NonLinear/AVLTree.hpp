ddf1e4eef1ef404d996b8ec8cc6d359c86238088
Bowen <tbow1382@740s-pl-120.local>
2019-04-30 08:17:13 -0600

    Created AVLTree.hpp and wrote the class section and some template methods.

---
+++ DataStructures/Model/NonLinear/AVLTree.hpp
@@ -0,0 +1,56 @@
+//
+//  AVLTree.h
+//  DataStructures
+//
+//  Created by Bowen, Tanner on 4/30/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef AVLTree_hpp
+#define AVLTree_hpp
+
+#include "BinarySearchTree.hpp"
+
+template <class Type>
+class AVLTree : public BinarySearchTree<Type>
+{
+private:
+    
+    BinartyTreeNode<Type> * leftRotation (BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * leftRightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightLeftRotation(BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * balanceSubTree (BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * insertNode(BinaryTreeNocde<Type> * parent, Type value);
+    BinaryTreeNode<Type> * removeNode(BinaryTreeNode<Type> * parent, Type value);
+    
+    int heightDifference(BinaryTreeNode<Type> * parent);
+    
+    public AVlTree();
+    
+    void insert(Type itemToInsert);
+    void remove(Type value);
+    
+};
+
+template <class Type>
+BinaryTreeNode<Type> * AVLTree<Type> :: insertNode(BinaryTreeNode<Type> * parent, Type inserted)
+{
+    
+}
+
+template <class Type>
+void AVLTree<Type> :: insert(Type item)
+{
+    insertNode(this->getRoot(), item);
+}
+
+template <class Type>
+void AVLTree<Type> :: remove(Type item)
+{
+    removeNode(this->getRoot(), item);
+}
+
+#endif /* AVLTree_hpp */
