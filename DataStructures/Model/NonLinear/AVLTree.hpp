663f690b68e6f1b8ca1eddc1d817f15ce7c98a88
Bowen <tbow1382@740s-pl-120.local>
2019-04-30 08:30:43 -0600

    Fixed errors, added a documentation, and wrote additional code for the insertNode method.

--- DataStructures/Model/NonLinear/AVLTree.hpp
+++ DataStructures/Model/NonLinear/AVLTree.hpp
@@ -1,5 +1,5 @@
 //
-//  AVLTree.h
+//  AVLTree.hpp
 //  DataStructures
 //
 //  Created by Bowen, Tanner on 4/30/19.
@@ -23,22 +23,45 @@ private:
     
     BinaryTreeNode<Type> * balanceSubTree (BinaryTreeNode<Type> * parent);
     
-    BinaryTreeNode<Type> * insertNode(BinaryTreeNocde<Type> * parent, Type value);
+    BinaryTreeNode<Type> * insertNode(BinaryTreeNode<Type> * parent, Type value);
     BinaryTreeNode<Type> * removeNode(BinaryTreeNode<Type> * parent, Type value);
     
     int heightDifference(BinaryTreeNode<Type> * parent);
     
-    public AVlTree();
+public:
+    AVLTree();
     
     void insert(Type itemToInsert);
     void remove(Type value);
     
 };
 
+/*
+ This method inserts a node as a parent upon the condition of the parent being a nullptr,
+ or inserts a node as a leftChild upon the condition of the node being inserted being less than the parent
+ or inserts a node as a rightChild upon the condition of the node being inserted being greater than the parent.
+ 
+ 
+ */
 template <class Type>
 BinaryTreeNode<Type> * AVLTree<Type> :: insertNode(BinaryTreeNode<Type> * parent, Type inserted)
 {
-    
+    if(parent == nullptr)
+    {
+        parent = new BinaryTreeNode<Type>(inserted);
+        this->setRoot(parent);
+        return parent;
+    }
+    else if(inserted < parent->getNodeData())
+    {
+        parent->setLeftChild(insertNode(parent->getLeftNode(), inserted));
+        parent = balanceSubTree(parent);
+    }
+    else if(inserted > parent->getNodeData())
+    {
+        parent->setRightChild(insertNode(parent->getRightNode(), inserted));
+    }
+    return parent;
 }
 
 template <class Type>
@@ -52,5 +75,9 @@ void AVLTree<Type> :: remove(Type item)
 {
     removeNode(this->getRoot(), item);
 }
+
+template <class Type>
+
+
 
 #endif /* AVLTree_hpp */
